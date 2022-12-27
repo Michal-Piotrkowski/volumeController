1731fc0fd9b9cdf8388aa71a162a8a40b84a335a
Michal-Piotrkowski <michal.piotrkowski@protonmail.com>
2022-12-27 19:42:54 +0100

    UwU commit

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume, ISimpleAudioVolume
 import tkinter as tk
-from tkinter import messagebox
 from tkinter import *
 from PIL import Image, ImageTk
 
@@ -17,18 +16,21 @@ class App():
     def create_App(self):
         ico = Image.open('volume.png')
         photo = ImageTk.PhotoImage(ico)
+        bg = PhotoImage(file="bg.png") #adding photo that will become the background
         self.root.wm_iconphoto(False, photo)
         self.root.title("Volume Master - management for volumes")
         self.width = self.root.winfo_screenwidth()
         self.height = self.root.winfo_screenheight()
-        self.root.geometry(f'{self.width // 2}x{self.height // 2}')
-        self.root.resizable(0, 0)
+        self.root.geometry(f'{int(self.width*0.65)}x{int(self.height*0.65)}')
+        label1 = Label(self.root, image=bg)
+        label1.place(x=0, y=0)
+        self.root.resizable(1, 1)
         self.sessions = AudioUtilities.GetAllSessions()
         for session in self.sessions:
             if session.Process:
-                num_input = Scale(self.root, from_=0, to=10, orient=VERTICAL)
-                num_input.pack(side=BOTTOM)
-                Button(self.root, text=f'{session.Process.name()}', command=lambda: self.show_values(int(num_input.get()))).pack(side=BOTTOM)
+                num_input = Scale(self.root, from_=0, to=10, sliderrelief='flat', orient="horizontal", highlightthickness=0, background='black', fg='grey', troughcolor='#73B5FA', activebackground='#1065BF')
+                num_input.pack(side=LEFT)
+                Button(self.root, text=f'{session.Process.name()}', command=lambda: self.show_values(int(num_input.get()))).pack(side=LEFT)
         self.root.mainloop()
     def show_values(self, num):
         volumeManager = VolumeManager()
